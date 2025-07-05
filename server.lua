5f5cb7d0a628d55c447fc93c75b13b857695dbaa
EJJ04 <144691106+EJJ04@users.noreply.github.com>
2025-07-06 01:49:10 +0200

    Delete server.lua

--- server.lua
+++
@@ -1,8 +0,0 @@
-AddEventHandler('onServerResourceStart', function(resourceName)
-    if resourceName == 'ox_inventory' or resourceName == GetCurrentResourceName() then
-        exports.ox_inventory:RegisterShop(Config.Name, {
-            name = "Black Market",
-            inventory = Config.Items
-        })
-    end
-end)
